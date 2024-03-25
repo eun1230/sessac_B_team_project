10d4ecc6ccc70f61e6f16782d4abc3555d0011de
eun1230 <jsily2288@naver.com>
2024-03-25 22:14:42 +0900

    feat : map+reserv

--- js/reservation.js
+++ js/reservation.js
@@ -1,5 +1,9 @@
 // 예약 완료 alert
 
-function clickEvent(){
-	alert('예약이 완료되었습니다!');
+function clickEvent() {
+  alert('예약이 완료되었습니다!');
 }
+let select = document.querySelector('#select');
+select.addEventListener('click', function () {
+  document.querySelector('.select').setAttribute('color', '#ff8f8f');
+});
